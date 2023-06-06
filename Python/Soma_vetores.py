e9386bebcac0bcae8a541e585635a2b63306f8bf
evanapimenta <70334054+evanapimenta@users.noreply.github.com>
2023-06-06 15:32:58 -0300

    .

--- Python/Soma_vetores.py
+++ Python/Soma_vetores.py
@@ -1,18 +1,18 @@
 # Escreva um algoritmo que leia dois vetores de 10 posições e faça a soma dos elementos de mesmo índice, colocando o resultado em um terceiro vetor. Mostre o vetor resultante.
 
-vector1 = []
-vector2 = []
-vector_soma = []
+vetor1 = []
+vetor2 = []
+vetor_soma = []
 i = 0
 
 while i < 10:
     value1 = int(input("Digite um valor para compor o primeiro vetor: "))
     value2 = int(input("Digite um valor para compor o segundo vetor: "))
-    vector1.append(value1)
-    vector2.append(value2)
-    vector_soma.append(value1+value2)
+    vetor1.append(value1)
+    vetor2.append(value2)
+    vetor_soma.append(value1+value2)
     i += 1
 
-print(vector1)
-print(vector2)
-print(vector_soma)
+print(vetor1)
+print(vetor2)
+print(vetor_soma)
